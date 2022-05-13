ea4f8534f21fc99713ee0c1d7ad39790b1b40097
Andrei Krutoy <andrei.krutoy1997@gmail.com>
2022-05-13 14:10:51 +0300

    Up

--- Task_1/main.py
+++ Task_1/main.py
@@ -1,4 +1,6 @@
 import time
+from datetime import datetime
+from prettytable import PrettyTable
 import psutil
 import os
 import sys
@@ -6,55 +8,70 @@ import subprocess
 
 platform = sys.platform
 
-path = input('Input path: ')
-delay = int(input('Input delay: '))
+path = input('input path: ')
+delay = int(input('input delay: '))
 
-if platform == 'Win-32':
+
+if platform == 'win32':
     os.startfile(path)
 
-    p_name = path.split('\\')[-1]
-    print(p_name)
+    process_name = path.split('\\')[-1]
+    print(process_name)
     try:
         for i in psutil.process_iter():
-            if i.name() == p_name:
-                with open('log.txt', 'a') as f:
+            if i.name() == process_name:
+                with open('E:\Pythonprojects\Test_Veeam_Case_QA\Task_1\log.txt', 'a') as f:
                     while True:
-                        f.write(f'CPU: {i.cpu_percent()}\n')
-                        print(f'CPU: {i.cpu_percent()}')  # The system-wide CPU utilization as a percentage
+                        now = datetime.now()
+                        current_time = now.strftime("%x %H:%M:%S")
 
-                        f.write(f'Working Set: {i.memory_full_info()[4]}\n')
-                        print(f'Working Set: {i.memory_full_info()[4]}') # Return a named tuple with representing memory information about the process
+                        f.write(f'Date: {current_time}\n')
+                        f.write(f'CPU: {i.cpu_percent(interval=0.0)} %\n')# The system-wide CPU utilization as a percentage
+                        f.write(f'Working Set: {i.memory_full_info()[4]}\n')# Return a named tuple with representing memory information about the process (WSET)
+                        f.write(f'Private Bytes: {i.memory_full_info()[-2]}\n')#Return a named tuple with representing memory information about the process (Private)
+                        f.write(f'handle: {i.num_handles()}\n')# The number of handles currently used by this process
+                        f.write('-' * 10)
+                        f.write('\n')
 
-                        f.write(f'Private Bytes: {i.memory_full_info()[-2]}\n')
-                        print(f'Privat Bytes: {i.memory_full_info()[-2]}')
+                        print(f'Date: {current_time}')
+                        print(f'CPU: {i.cpu_percent(interval=0.5)} %')
+                        print(f'Working Set: {i.memory_full_info()[4]}')
+                        print(f'Private Bytes: {i.memory_full_info()[-2]}')
+                        print(f'handle: {i.num_handles()}')
+                        print('-' * 10)
 
-                        f.write(f'Handle: {i.num_handles()}\n')
-                        print(f'Hendle: {i.num_handles()}') # The number of handles currently used by this process
-
+                        #print(i.memory_full_info()) # Get tuple
                         time.sleep(delay)
     except:
         pass
 
 else:
     subprocess.Popen(path)
-    p_name = path.split('/')[-1]
+    process_name = path.split('/')[-1]
     try:
         for i in psutil.process_iter():
-            if i.name() == p_name:
-                with open('log.txt', 'a') as f:
+            if i.name() == process_name:
+                with open('E:\Pythonprojects\Test_Veeam_Case_QA\Task_1\log.txt', 'a') as f:
                     while True:
-                        f.write(f'CPU: {i.cpu_percent()}\n')
-                        print(f'CPU: {i.cpu_percent()}')
+                        now = datetime.now()
+                        current_time = now.strftime("%x %H:%M:%S")
 
-                        f.write(f'Resident set size: {i.memory_full_info()[0]}\n')
-                        print(f'Resident set size: {i.memory_full_info()[0]}')
+                        f.write(f'Date: {current_time}\n')
+                        f.write(f'CPU: {i.cpu_percent()} %\n')
+                        f.write(f'Resident Set Size: {i.memory_full_info()[0]}\n')#Return a named tuple with representing memory information about the process (RSS)
+                        f.write(f'Virtual Memory Size: {i.memory_full_info()[1]}\n')#Return a named tuple with representing memory information about the process (VMS)
+                        f.write(f'Descriptors: {i.num_fds()}\n')
+                        f.write('-' * 10)
+                        f.write('\n')
 
-                        f.write(f'Virtual memory size: {i.memory_full_info()[1]}\n')
-                        print(f'Virtual menory size: {i.memory_full_info()[1]}')
-
-                        f.write(f'Descriptor: {i.num_fds()}\m')
-                        print(f'Descriptor: {i.num_fds}') # The number of file descriptors currently opened by this process
+                        print(f'Date: {current_time}')
+                        print(f'CPU: {i.cpu_percent()} %')
+                        print(f'Resident Set Size: {i.memory_full_info()[0]}')
+                        print(f'Virtual Memory Size: {i.memory_full_info()[1]}')
+                        print(f'Descriptors: {i.num_fds()}') # The number of file descriptors currently opened by this process
+                        print('-' * 10)
 
                         time.sleep(delay)
     except:
         pass
+
